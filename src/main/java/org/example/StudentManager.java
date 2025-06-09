86b72237609f53525d07a814de67fbdfe25a6d5e
kasularoja <kasularoja67@gmail.com>
2025-06-08 20:37:46 -0500

    basics4

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -35,7 +35,9 @@ public class StudentManager {
         // Prompt the user to enter a new student name (using scanner and I/O methods learned previously,
         // refer to mainMenu() for an example)
         // Add the student to the list
-
+        System.out.println("Enter student name to add: ");
+        String studentName = scanner.nextLine();
+        System.out.println(studentName + " has been added.");
         mainMenu();
     }
 
@@ -43,13 +45,31 @@ public class StudentManager {
         // Prompt the user for a student name
         // Use the contains method to check if the student entered is in the list
         // If so, remove it, if not, print "Student not found."
-
+        System.out.println("Enter student name to remove: ");
+        String studentName = scanner.nextLine();
+        if (students.contains(studentName)) {
+            students.remove(studentName);
+            System.out.println("Student " + studentName + " has been removed.");
+        } else {
+            System.out.println("Student not found.");
+        }
         mainMenu();
     }
+
 
     public void viewStudents() {
         // Loop through the list of students and print each one
         // (Use a for-each loop!)
+        if (students.isEmpty()) {
+            System.out.println("No students found.");
+        } else {
+            System.out.println("Students: ");
+            for (String student : students) {
+                System.out.println(student);
+            }
+
+        }
         mainMenu();
     }
 }
+
